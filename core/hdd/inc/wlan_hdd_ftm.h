b44b1b5f25d6c8744bd2cd1ee23533e84b0f138a
Ranjeet Gupta <fpx478@motorola.com>
2017-04-26 12:23:25 -0500

    - Whitelist the Rx commands
    Change-Id: I62fc0845e8031e24752b65755323b43a71a870bc
    Reviewed-on: https://gerrit.mot.com/985716
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ranjeet Gupta <ranjeetgupta@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- core/hdd/inc/wlan_hdd_ftm.h
+++ core/hdd/inc/wlan_hdd_ftm.h
@@ -44,7 +44,8 @@
 //A TLV stream contains a 28-byte stream header, and its payload. It represents
 //a command from host or a response from target.
 #define WLAN_FTM_OPCODE_TX_ON 28
-#define WLAN_FTM_OPCODE_PHY_ON 40
+#define WLAN_FTM_OPCODE_PHY   40
+#define WLAN_FTM_OPCODE_DATA  52
 
 enum wlan_hdd_ftm_state {
 	WLAN_FTM_INITIALIZED,
