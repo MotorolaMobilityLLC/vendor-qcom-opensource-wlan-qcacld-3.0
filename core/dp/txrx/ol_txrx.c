c9f85077687127c892f9bcd9943bcce7458cfa52
Himanshu Agarwal <himanaga@codeaurora.org>
2017-04-27 15:41:29 +0530

    qcacld-3.0: Check for peer validity before enqueueing frames
    There can be a race condition happening where frames are being
    enqueued in cached buf queue for a peer in one thread whereas
    cached buf queue for that same peer is being flushed in another
    thread at the same time. This could result in memory leak in buf
    queue.
    
    Check for peer validity before enqueueing frames in peer cached
    buf queue.
    
    Change-Id: I6663b93a0b4764b2775de54183aca1a77b0ed125
    CRs-fixed: 2038457

--- core/dp/txrx/ol_txrx.c
+++ core/dp/txrx/ol_txrx.c
@@ -4776,6 +4776,7 @@ free_buf:
 #define OL_TXRX_PRINT_RATE_LIMIT_THRESH 0x0f
 
 static QDF_STATUS ol_txrx_enqueue_rx_frames(
+					struct ol_txrx_peer_t *peer,
 					struct ol_txrx_cached_bufq_t *bufqi,
 					qdf_nbuf_t rx_buf_list)
 {
@@ -4784,8 +4785,6 @@ static QDF_STATUS ol_txrx_enqueue_rx_frames(
 	QDF_STATUS status = QDF_STATUS_SUCCESS;
 	int dropped = 0;
 	static uint32_t count;
-	bool thresh_crossed = false;
-
 
 	if ((count++ & OL_TXRX_PRINT_RATE_LIMIT_THRESH) == 0)
 		TXRX_PRINT(TXRX_PRINT_LEVEL_INFO1,
@@ -4795,20 +4794,9 @@ static QDF_STATUS ol_txrx_enqueue_rx_frames(
 	qdf_spin_lock_bh(&bufqi->bufq_lock);
 	if (bufqi->curr >= bufqi->thresh) {
 		status = QDF_STATUS_E_FAULT;
-		dropped = ol_txrx_drop_nbuf_list(rx_buf_list);
-		bufqi->dropped += dropped;
-		bufqi->qdepth_no_thresh += dropped;
-
-		if (bufqi->qdepth_no_thresh > bufqi->high_water_mark)
-			bufqi->high_water_mark = bufqi->qdepth_no_thresh;
-
-		thresh_crossed = true;
+		goto drop_bufs;
 	}
-
 	qdf_spin_unlock_bh(&bufqi->bufq_lock);
-
-	if (thresh_crossed)
-		goto end;
 
 	buf = rx_buf_list;
 	while (buf) {
@@ -4824,14 +4812,30 @@ static QDF_STATUS ol_txrx_enqueue_rx_frames(
 			qdf_nbuf_set_next(buf, NULL);
 			cache_buf->buf = buf;
 			qdf_spin_lock_bh(&bufqi->bufq_lock);
-			list_add_tail(&cache_buf->list,
+			if (peer && peer->valid) {
+				list_add_tail(&cache_buf->list,
 				      &bufqi->cached_bufq);
-			bufqi->curr++;
-			qdf_spin_unlock_bh(&bufqi->bufq_lock);
+				bufqi->curr++;
+				qdf_spin_unlock_bh(&bufqi->bufq_lock);
+			} else {
+				status = QDF_STATUS_E_FAULT;
+				qdf_mem_free(cache_buf);
+				rx_buf_list = buf;
+				qdf_nbuf_set_next(rx_buf_list, next_buf);
+				goto drop_bufs;
+			}
 		}
 		buf = next_buf;
 	}
-end:
+
+drop_bufs:
+	dropped = ol_txrx_drop_nbuf_list(rx_buf_list);
+	bufqi->dropped += dropped;
+	bufqi->qdepth_no_thresh += dropped;
+
+	if (bufqi->qdepth_no_thresh > bufqi->high_water_mark)
+		bufqi->high_water_mark = bufqi->qdepth_no_thresh;
+	qdf_spin_unlock_bh(&bufqi->bufq_lock);
 	return status;
 }
 /**
@@ -4869,7 +4873,8 @@ void ol_rx_data_process(struct ol_txrx_peer_t *peer,
 	 * which will be flushed to HDD once that station is registered.
 	 */
 	if (!data_rx) {
-		if (ol_txrx_enqueue_rx_frames(&peer->bufq_info, rx_buf_list)
+		if (ol_txrx_enqueue_rx_frames(peer, &peer->bufq_info,
+					      rx_buf_list)
 				!= QDF_STATUS_SUCCESS)
 			QDF_TRACE(QDF_MODULE_ID_TXRX, QDF_TRACE_LEVEL_INFO_HIGH,
 				  "%s: failed to enqueue rx frm to cached_bufq",
