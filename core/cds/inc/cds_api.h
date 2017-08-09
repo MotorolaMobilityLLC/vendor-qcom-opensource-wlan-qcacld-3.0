arter97/caf_qcacld-3.0

qcacld-3.0: Add protection for pkt_log ops with module stop
Packet log ops are protected against load-unload and SSR, but not
against module stop. Take care to add check for module stop.
Also takecare to NULLing of wdi_event_list and NULL check before accessing.

Change-Id: I20d49ff587719fc14f60a53e86092383de6ef5b8
CRs-Fixed: 2082135

core/cds/inc/cds_api.h
@@ -63,13 +63,15 @@
  * CDS_DRIVER_STATE_LOADING: Driver probe is in progress.
  * CDS_DRIVER_STATE_UNLOADING: Driver remove is in progress.
  * CDS_DRIVER_STATE_RECOVERING: Recovery in progress.
+ * CDS_DRIVER_STATE_MODULE_STOPPING: Module stop in progress.
  */
 enum cds_driver_state {
-	CDS_DRIVER_STATE_UNINITIALIZED	= 0,
-	CDS_DRIVER_STATE_LOADED		= BIT(0),
-	CDS_DRIVER_STATE_LOADING	= BIT(1),
-	CDS_DRIVER_STATE_UNLOADING	= BIT(2),
-	CDS_DRIVER_STATE_RECOVERING	= BIT(3),
+	CDS_DRIVER_STATE_UNINITIALIZED		= 0,
+	CDS_DRIVER_STATE_LOADED			= BIT(0),
+	CDS_DRIVER_STATE_LOADING		= BIT(1),
+	CDS_DRIVER_STATE_UNLOADING		= BIT(2),
+	CDS_DRIVER_STATE_RECOVERING		= BIT(3),
+	CDS_DRIVER_STATE_MODULE_STOPPING	= BIT(4),
 };
 
 #define __CDS_IS_DRIVER_STATE(_state, _mask) (((_state) & (_mask)) == (_mask))
@@ -198,6 +200,38 @@ static inline bool cds_is_load_or_unload_in_progress(void)
 }
 
 /**
+ * cds_is_module_stop_in_progress() - Is module stopping
+ *
+ * Return: true if module stop is in progress.
+ */
+static inline bool cds_is_module_stop_in_progress(void)
+{
+	enum cds_driver_state state = cds_get_driver_state();
+
+	return __CDS_IS_DRIVER_STATE(state, CDS_DRIVER_STATE_MODULE_STOPPING);
+}
+
+/**
+ * cds_is_module_state_transitioning() - Is module state transitioning
+ *
+ * Return: true if module stop is in progress.
+ */
+static inline int cds_is_module_state_transitioning(void)
+{
+	if (cds_is_load_or_unload_in_progress() || cds_is_driver_recovering() ||
+		cds_is_module_stop_in_progress()) {
+		pr_info("%s: Load/Unload %d or recovery %d or module_stop %d is in progress",
+			__func__, cds_is_load_or_unload_in_progress(),
+				cds_is_driver_recovering(),
+				cds_is_module_stop_in_progress());
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+/**
  * cds_is_fw_down() - Is FW down or not
  *
  * Return: true if FW is down and false otherwise.
@@ -263,6 +297,21 @@ static inline void cds_set_unload_in_progress(uint8_t value)
 		cds_set_driver_state(CDS_DRIVER_STATE_UNLOADING);
 	else
 		cds_clear_driver_state(CDS_DRIVER_STATE_UNLOADING);
+}
+
+/**
+ * cds_set_module_stop_in_progress() - Setting module stop in progress
+ *
+ * @value: value to set
+ *
+ * Return: none
+ */
+static inline void cds_set_module_stop_in_progress(bool value)
+{
+	if (value)
+		cds_set_driver_state(CDS_DRIVER_STATE_MODULE_STOPPING);
+	else
+		cds_clear_driver_state(CDS_DRIVER_STATE_MODULE_STOPPING);
 }
 
 /**

MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0

core/cds/inc/cds_api.h
@@ -64,15 +64,18 @@
  * CDS_DRIVER_STATE_UNLOADING: Driver remove is in progress.
  * CDS_DRIVER_STATE_RECOVERING: Recovery in progress.
  * CDS_DRIVER_STATE_BAD: Driver in bad state.
+ * CDS_DRIVER_STATE_FW_READY:
+ * CDS_DRIVER_STATE_MODULE_STOPPING: Module stop in progress.
  */
 enum cds_driver_state {
-	CDS_DRIVER_STATE_UNINITIALIZED	= 0,
-	CDS_DRIVER_STATE_LOADED		= BIT(0),
-	CDS_DRIVER_STATE_LOADING	= BIT(1),
-	CDS_DRIVER_STATE_UNLOADING	= BIT(2),
-	CDS_DRIVER_STATE_RECOVERING	= BIT(3),
-	CDS_DRIVER_STATE_BAD		= BIT(4),
-	CDS_DRIVER_STATE_FW_READY	= BIT(5),
+	CDS_DRIVER_STATE_UNINITIALIZED	 = 0,
+	CDS_DRIVER_STATE_LOADED		 = BIT(0),
+	CDS_DRIVER_STATE_LOADING	 = BIT(1),
+	CDS_DRIVER_STATE_UNLOADING	 = BIT(2),
+	CDS_DRIVER_STATE_RECOVERING	 = BIT(3),
+	CDS_DRIVER_STATE_BAD		 = BIT(4),
+	CDS_DRIVER_STATE_FW_READY	 = BIT(5),
+	CDS_DRIVER_STATE_MODULE_STOPPING = BIT(6),
 };
 
 #define __CDS_IS_DRIVER_STATE(_state, _mask) (((_state) & (_mask)) == (_mask))
@@ -214,6 +217,38 @@ static inline bool cds_is_load_or_unload_in_progress(void)
 }
 
 /**
+ * cds_is_module_stop_in_progress() - Is module stopping
+ *
+ * Return: true if module stop is in progress.
+ */
+static inline bool cds_is_module_stop_in_progress(void)
+{
+	enum cds_driver_state state = cds_get_driver_state();
+
+	return __CDS_IS_DRIVER_STATE(state, CDS_DRIVER_STATE_MODULE_STOPPING);
+}
+
+/**
+ * cds_is_module_state_transitioning() - Is module state transitioning
+ *
+ * Return: true if module stop is in progress.
+ */
+static inline int cds_is_module_state_transitioning(void)
+{
+	if (cds_is_load_or_unload_in_progress() || cds_is_driver_recovering() ||
+		cds_is_module_stop_in_progress()) {
+		pr_info("%s: Load/Unload %d or recovery %d or module_stop %d is in progress",
+			__func__, cds_is_load_or_unload_in_progress(),
+				cds_is_driver_recovering(),
+				cds_is_module_stop_in_progress());
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+/**
  * cds_is_fw_down() - Is FW down or not
  *
  * Return: true if FW is down and false otherwise.
@@ -319,6 +354,21 @@ static inline void cds_set_unload_in_progress(uint8_t value)
 		cds_set_driver_state(CDS_DRIVER_STATE_UNLOADING);
 	else
 		cds_clear_driver_state(CDS_DRIVER_STATE_UNLOADING);
+}
+
+/**
+ * cds_set_module_stop_in_progress() - Setting module stop in progress
+ *
+ * @value: value to set
+ *
+ * Return: none
+ */
+static inline void cds_set_module_stop_in_progress(bool value)
+{
+	if (value)
+		cds_set_driver_state(CDS_DRIVER_STATE_MODULE_STOPPING);
+	else
+		cds_clear_driver_state(CDS_DRIVER_STATE_MODULE_STOPPING);
 }
 
 /**