8e44dd981331e65bfe17fd372b0bd4967e64479f
Srikanth Marepalli <marepall@codeaurora.org>
2020-06-11 12:33:33 +0530

    qcacld-3.0: Handle OWE info update from hostapd
    Handle OWE info update from hostapd in the following path:
    HDD->SAP->SME->CSR
    
    Moto-CRs-Fixed: (CR)
    
    Change-Id: I7584fb5db036d1643bd7d56076d30a0c75008a98
    CRs-Fixed: 2397009
    Reviewed-on: https://gerrit.mot.com/1719170
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- core/sme/src/common/sme_api.c
+++ core/sme/src/common/sme_api.c
@@ -17296,3 +17296,9 @@ error:
 
 	return qdf_status;
 }
+
+QDF_STATUS sme_update_owe_info(tpAniSirGlobal mac,
+			       tSirSmeAssocInd *assoc_ind)
+{
+	return csr_update_owe_info(mac, assoc_ind);
+}
