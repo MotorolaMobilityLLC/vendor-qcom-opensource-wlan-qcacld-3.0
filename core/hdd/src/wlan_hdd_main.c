1f226e6ac0e181b21148e7c254ad0dad0864bd4d
xiangming1 <xiangming1@lenovo.com>
2021-08-02 17:41:09 +0800

    Add new sysfs entries for SAR cutback
    Create sar_mhs and sar_sta sysfs entries for recording wlan states
    for SAR cutback. The WLAN driver does not care about these entries
    but they are needed as IPC over to the mdmcutback daemon.
    
    Change-Id: Ia23113813604f81f16eaeff5de1804aad1d2edf2
    Signed-off-by: xiangming1 <xiangming1@mt.com>
    Reviewed-on: https://gerrit.mot.com/2030555
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ningkai Zhuang <zhuangnk1@mt.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Ming Xiang <xiangmin@motorola.com>

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -296,6 +296,17 @@ static int enable_11d = -1;
 static int enable_dfs_chan_scan = -1;
 static bool is_mode_change_psoc_idle_shutdown;
 
+#define BUF_LEN_SAR 10
+static char  sar_sta_buffer[BUF_LEN_SAR];
+static struct kparam_string sar_sta = {
+	.string = sar_sta_buffer,
+	.maxlen = BUF_LEN_SAR,
+};
+static char  sar_mhs_buffer[BUF_LEN_SAR];
+static struct kparam_string sar_mhs = {
+	.string = sar_mhs_buffer,
+	.maxlen = BUF_LEN_SAR,
+};
 #define WLAN_NLINK_CESIUM 30
 
 static qdf_wake_lock_t wlan_wake_lock;
@@ -1142,7 +1153,8 @@ EXPORT_SYMBOL(con_mode_ftm);
 #endif
 int con_mode_epping;
 
-static int pcie_gen_speed;
+//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
+//static int pcie_gen_speed;
 
 /* Variable to hold connection mode including module parameter con_mode */
 static int curr_con_mode;
@@ -17278,6 +17290,11 @@ exit:
 	return errno;
 }
 
+static int sar_changed_handler(const char *kmessage,
+                                const struct kernel_param *kp)
+{
+        return param_set_copystring(kmessage, kp);
+}
 static int hdd_set_con_mode(enum QDF_GLOBAL_MODE mode)
 {
 	con_mode = mode;
@@ -19258,6 +19275,8 @@ static const struct kernel_param_ops fwpath_ops = {
 	.get = param_get_string,
 };
 
+//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
+/*
 static int __pcie_set_gen_speed_handler(void)
 {
 	int ret;
@@ -19302,10 +19321,11 @@ out:
 
 	return ret;
 }
+*/
 
-static const struct kernel_param_ops pcie_gen_speed_ops = {
-	.set = pcie_set_gen_speed_handler,
-	.get = param_get_int,
+static const struct kernel_param_ops sar_ops = {
+	.set = sar_changed_handler,
+	.get = param_get_string,
 };
 
 module_param_cb(con_mode, &con_mode_ops, &con_mode,
@@ -19314,8 +19334,11 @@ module_param_cb(con_mode, &con_mode_ops, &con_mode,
 module_param_cb(con_mode_ftm, &con_mode_ftm_ops, &con_mode_ftm,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
+//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
+/*
 module_param_cb(pcie_gen_speed, &pcie_gen_speed_ops, &pcie_gen_speed,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+*/
 
 #ifdef WLAN_FEATURE_EPPING
 module_param_cb(con_mode_epping, &con_mode_epping_ops,
@@ -19324,6 +19347,11 @@ module_param_cb(con_mode_epping, &con_mode_epping_ops,
 
 module_param_cb(fwpath, &fwpath_ops, &fwpath,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+module_param_cb(sar_sta, &sar_ops, &sar_sta,
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+module_param_cb(sar_mhs, &sar_ops, &sar_mhs,
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
 
 module_param(enable_dfs_chan_scan, int, S_IRUSR | S_IRGRP | S_IROTH);
 
