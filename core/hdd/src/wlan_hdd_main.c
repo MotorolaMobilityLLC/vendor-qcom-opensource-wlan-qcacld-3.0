5944c395cd95e8b12f237fb3abbf5fcf2eb0ce38
gaoyx9 <gaoyx9@motorola.com>
2022-03-14 02:57:37 -0600

    Revert "(CR) : [Rhode5G][factory][DVT2][AR] WLAN 2.4&5.0 GHz Test fail"
    This reverts commit 17bf99a8ff6723d9491e2263d2f1c2bf91b76e13.
    
    Change-Id: Icad84d171686655c85787e250e4c1dadb96ad5e0
    Reviewed-on: https://gerrit.mot.com/2215094
    Reviewed-by: gaoyx9 <gaoyx9@motorola.com>
    Submit-Approved: gaoyx9 <gaoyx9@motorola.com>
    Tested-by: gaoyx9 <gaoyx9@motorola.com>
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -12279,9 +12279,9 @@ int hdd_psoc_idle_shutdown(struct device *dev)
 	 * received from modem subsystem in wearable devices
 	 */
 	if (hdd_is_any_interface_open(hdd_ctx)) {
-		hdd_err_rl("all interfaces are not down, ignore idle shutdown.now we don't return here temp");
+		hdd_err_rl("all interfaces are not down, ignore idle shutdown");
+		return -EINVAL;
 	}
-
 	if (is_mode_change_psoc_idle_shutdown)
 		ret = __hdd_mode_change_psoc_idle_shutdown(hdd_ctx);
 	else
