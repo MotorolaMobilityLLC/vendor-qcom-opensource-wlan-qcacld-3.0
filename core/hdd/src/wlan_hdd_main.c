abf407987fca4c93017864d69d5f9f23abe42ba1
zhaoqh3 <zhaoqh3@lenovo.com>
2022-07-11 16:43:24 +0800

    Wifi SAR porting.
    Revert the change (CR).
    It doesn't need to remove QC related codes to fix build error. We just
    need to add moto SAR related function definition.
    
    Change-Id: I722c201a05529c1a2789b21664beca7f7651ba94
    Signed-off-by: zhaoqh3 <zhaoqh3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2329181
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yue Sun <sunyue5@lenovo.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2773200
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3069254
    Reviewed-by: Rui Hu <hurui1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3427551

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -1239,8 +1239,7 @@ EXPORT_SYMBOL(con_mode_ftm);
 #endif
 int con_mode_epping;
 
-//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
-//static int pcie_gen_speed;
+static int pcie_gen_speed;
 
 /* Variable to hold connection mode including module parameter con_mode */
 static int curr_con_mode;
@@ -24069,8 +24068,6 @@ static const struct kernel_param_ops fwpath_ops = {
 	.get = param_get_string,
 };
 
-//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
-/*
 static int __pcie_set_gen_speed_handler(void)
 {
 	int ret;
@@ -24115,7 +24112,11 @@ out:
 
 	return ret;
 }
-*/
+
+static const struct kernel_param_ops pcie_gen_speed_ops = {
+	.set = pcie_set_gen_speed_handler,
+	.get = param_get_int,
+};
 
 static const struct kernel_param_ops sar_ops = {
 	.set = sar_changed_handler,
@@ -24128,11 +24129,8 @@ module_param_cb(con_mode, &con_mode_ops, &con_mode,
 module_param_cb(con_mode_ftm, &con_mode_ftm_ops, &con_mode_ftm,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
-//IKSWR-4630 Removed unused code because it will result in compilation error at Kernel 5.4
-/*
 module_param_cb(pcie_gen_speed, &pcie_gen_speed_ops, &pcie_gen_speed,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-*/
 
 #ifdef WLAN_FEATURE_EPPING
 module_param_cb(con_mode_epping, &con_mode_epping_ops,
