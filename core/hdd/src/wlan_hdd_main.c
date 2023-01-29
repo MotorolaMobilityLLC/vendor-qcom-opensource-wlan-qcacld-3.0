6e25b8ee8ff6e4ae6ee25df2c2f5bb3ee7028d7a
zhuangnk1 <zhuangnk1@lenovo.com>
2023-01-29 14:55:46 +0800

    : Add new ini file for PRC based on radio
    PRC product may be need some more aggressive parameters
    so we will add new ini file named WCNSS_prc_cfg.xx.ini
    it will only work for PRC product
    
    Change-Id: Icd225416484365644a1f14c452e77552d953ce9c
    Signed-off-by: zhuangnk1 <zhuangnk1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2823835
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiuhuan Zhao <zhaoqh3@lenovo.com>
    Reviewed-by: Yue Sun <sunyue5@lenovo.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3069266
    Reviewed-by: Rui Hu <hurui1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3427517

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -16004,6 +16004,69 @@ static inline QDF_STATUS hdd_cfg_parse_connection_roaming_cfg(void)
 }
 #endif
 
+/*Begin Add moto PRC special ini overlay*/
+#define MOTO_STRING_LEN 32
+static char *bootargs_str;
+static char radio_ptr[MOTO_STRING_LEN] = {0};
+
+static int wlan_get_bootarg_dt(char *key, char **value, char *prop, char *spl_flag)
+{
+	const char *bootargs_tmp = NULL;
+	char *idx = NULL;
+	char *kvpair = NULL;
+	int err = 1;
+	struct device_node *n = of_find_node_by_path("/chosen");
+	size_t bootargs_tmp_len = 0;
+
+	if (n == NULL)
+		goto err;
+
+	if (of_property_read_string(n, prop, &bootargs_tmp) != 0)
+		goto putnode;
+
+	bootargs_tmp_len = strlen(bootargs_tmp);
+	if (!bootargs_str) {
+		/* The following operations need a non-const
+		 * version of bootargs
+		*/
+		bootargs_str = kzalloc(bootargs_tmp_len + 1, GFP_KERNEL);
+		if (!bootargs_str)
+			goto putnode;
+	}
+	strscpy(bootargs_str, bootargs_tmp, bootargs_tmp_len + 1);
+
+	idx = strnstr(bootargs_str, key, strlen(bootargs_str));
+	if (idx) {
+		kvpair = strsep(&idx, " ");
+		if (kvpair)
+			if (strsep(&kvpair, "=")) {
+				*value = strsep(&kvpair, spl_flag);
+				if (*value)
+					err = 0;
+			}
+	}
+
+putnode:
+	of_node_put(n);
+err:
+	return err;
+}
+
+static int get_moto_radio(void)
+{
+	char *radiodevice = NULL;
+	int rc = 0;
+	rc = wlan_get_bootarg_dt("androidboot.radio=", &radiodevice, "mmi,bootconfig", "\n");
+	if (rc || !radiodevice){
+		hdd_err("radio string is error");
+		return -ENOMEM;
+	}else{
+		strscpy(radio_ptr, radiodevice, MOTO_STRING_LEN);
+		return 0;
+	}
+}
+/*End Add moto PRC special ini overlay*/
+
 struct hdd_context *hdd_context_create(struct device *dev)
 {
 	QDF_STATUS status;
@@ -16046,6 +16109,21 @@ struct hdd_context *hdd_context_create(struct device *dev)
 		ret = qdf_status_to_os_return(status);
 		goto err_free_config;
 	}
+
+	/*Begin,IKSWU-42693,hurui1,Add moto PRC special ini overlay*/
+	if(get_moto_radio() != 0) {
+		hdd_err("radio not present");
+	} else {
+		hdd_debug("radio %s",radio_ptr);
+		if (strcmp(radio_ptr, "PRC") == 0) {
+			status = cfg_parse(WLAN_PRC_INI_FILE);
+			if (QDF_IS_STATUS_ERROR(status)) {
+				hdd_err("Failed to parse cfg %s, skip!",
+				WLAN_PRC_INI_FILE);
+			}
+		}
+	}
+	/*End,hurui1,IKSWU-42693*/
 
 	status = hdd_cfg_parse_connection_roaming_cfg();
 
