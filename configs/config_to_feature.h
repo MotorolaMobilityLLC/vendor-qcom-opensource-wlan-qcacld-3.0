29fb3f67885e0278fac09b8721bdbf7858a4b33a
Haichao Li <lihc3@lenovo.com>
2023-12-29 10:41:33 +0800

    utags:add moto config to bazel:config_to_feature.h
    QC introduce Bazel instead original kernel make system
    add Moto config to bazel feature header:config_to:feature.h
    
    Change-Id: I92d5e01c3240e41cf5df8f67a837de4f0e2db6ef
    Signed-off-by: Haichao Li <lihc3@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2839611
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiuhuan Zhao <zhaoqh3@lenovo.com>
    Reviewed-by: Ningkai Zhuang <zhuangnk1@lenovo.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/3069314
    Reviewed-by: Rui Hu <hurui1@motorola.com>

--- configs/config_to_feature.h
+++ configs/config_to_feature.h
@@ -3028,6 +3028,9 @@
 #define WLAN_BCN_RATECODE_ENABLE (1)
 #endif
 
+/* move utags build flags from kbuild to here */
+#define MOTO_UTAGS_MAC (1)
+
 #ifdef CONFIG_WLAN_SUPPORT_SERVICE_CLASS
 #define WLAN_SUPPORT_SERVICE_CLASS (1)
 #endif
