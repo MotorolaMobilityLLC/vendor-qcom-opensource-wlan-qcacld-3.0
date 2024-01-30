d861b6d520cd42f7dd43f6cf44ce9e6e7a105f68
Yue Sun <sunyue5@motorola.com>
2024-01-30 15:01:26 +0800

    wlan: Enable scan request in ULL mode
    In the latest GMS APPs l(CR) Android Auto and NearbyShare, it
    usually set ULL mode and scanning at the same time. Since QC
    default settings in the ULL mode will avoid scan request, DUT
    will fail to connect to peer.
    Therefore, we have to allow scan request in the ULL mode.
    
    Change-Id: I128874d98b098bae91e01d614af6b12687a8039e
    Signed-off-by: Yue Sun <sunyue5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2868996
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Qiuhuan Zhao <zhaoqh3@lenovo.com>
    Reviewed-by: <lixt11@lenovo.com>
    Reviewed-by: Zhilu Yin <yinzl1@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    (cherry picked from commit d040bd753627e9874a5e0ffb32d8be80593ca990)
    Reviewed-on: https://gerrit.mot.com/2874572
    Reviewed-by: Yue Sun <sunyue5@lenovo.com>
    Reviewed-by: Haichao Li <liuhc3@motorola.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>

--- components/mlme/dispatcher/inc/cfg_mlme_fe_wlm.h
+++ components/mlme/dispatcher/inc/cfg_mlme_fe_wlm.h
@@ -361,7 +361,7 @@
  *
  * </ini>
  */
-#define CFG_DEFAULT_ULTLOW_FLAGS "0xc83"
+#define CFG_DEFAULT_ULTLOW_FLAGS "0xc82"	// IKSWU-66655, allow scan request in ULL mode
 #define CFG_LATENCY_FLAGS_ULTLOW \
 		CFG_INI_STRING("wlm_latency_flags_ultralow",\
 			       0, \
