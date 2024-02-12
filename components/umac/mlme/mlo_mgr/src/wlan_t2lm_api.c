200c7fd3d1e5d9becddd75d636374676892d4fb9
Deeksha Gupta <quic_deegupta@quicinc.com>
2024-02-12 14:35:15 +0530

    qcacld-3.0: Fix out-of-bound in wlan_mlo_parse_bcn_prbresp_t2lm_ie
    Currently, In the MLO t2lm API, wlan_mlo_parse_bcn_prbresp_t2lm_ie
    is missing frame boundary checks which may lead to out-of-bound
    reads if the lengths are not checked by the caller.
    
    Fix is, while parsing t2lm ie pass the frame length and add
    check for frame boundary.
    
    Mot-CRs-fixed: (CR)
    CVE- fixed: CVE-2024-33048
    
    CRs-Fixed: 3707241
    Change-Id: Ic83638eff2250a704df8dfa8bd233238fcc7a25b
    Signed-off-by: Dharshan R <dharshan@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3061203
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- components/umac/mlme/mlo_mgr/src/wlan_t2lm_api.c
+++ components/umac/mlme/mlo_mgr/src/wlan_t2lm_api.c
@@ -504,7 +504,8 @@ wlan_t2lm_validate_candidate(struct cnx_mgr *cm_ctx,
 	}
 
 	status = wlan_mlo_parse_bcn_prbresp_t2lm_ie(&t2lm_ctx,
-						    scan_entry->ie_list.t2lm[0]);
+	                                util_scan_entry_t2lm(scan_entry),
+					util_scan_entry_t2lm_len(scan_entry));
 	if (QDF_IS_STATUS_ERROR(status))
 		goto end;
 
