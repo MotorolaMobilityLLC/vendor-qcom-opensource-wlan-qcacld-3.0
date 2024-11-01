e04412c9c22a388e39c56a58f58250b1ad83a16a
Zhiye Li <lizy90@motorola.com>
2024-11-01 18:57:14 +0800

    Revert "(CR):qcacld-3.0:WCNSS_mot_cfg.ini not load in parrot"
    /sys/module/qca_cld3_qca6750/parameters/con_mode
    not exist, result the tcmd wlan firmware test fail:
    TCMDHELP: echo 5 > /sys/module/wlan/parameters/con_mode
    failed. system_val = 256, exit_val = 1
    
    Change-Id: I88f031f2cf6899273e94ab6b7c759d1916b29e12
    Reviewed-on: https://gerrit.mot.com/3132609
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Yue Sun <sunyue5@lenovo.com>
    Reviewed-by: Tao Sun <suntao2@motorola.com>
    Submit-Approved: Jira Key

--- Android.mk
+++ Android.mk
@@ -30,7 +30,7 @@ endif
 
 ifeq ($(TARGET_BOARD_PLATFORM), parrot)
 ifeq ($(TARGET_BOARD_SUFFIX),66)
-# LOCAL_MODULE_DDK_BUILD := true
+LOCAL_MODULE_DDK_BUILD := true
 LOCAL_MODULE_DDK_ALLOW_UNSAFE_HEADERS := true
 endif
 endif
