3a6e708734b6120a0eb206272a3bc24d6ebb249b
Asutosh Mohapatra <asutmoha@qti.qualcomm.com>
2025-11-24 22:54:44 -0800

    qcacld-3.0: Skip GTK offload in open mode on suspend/resume
    Do not enable/disable GTK offload during PM when connected in
    open mode. GTK offload is only relevant for encrypted links;
    issuing it in open mode causes unnecessary error prints.
    
    Mot-CRs-Fixed: (CR)
    
    CRs-Fixed: 4363222
    Change-Id: I62c8806d80eb86db4085d72b897274a863191197
    Reviewed-on: https://gerrit.mot.com/3654635
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Reviewed-by: Obi-Wan Kenobi
    Submit-Approved: Jira Key

--- core/hdd/src/wlan_hdd_power.c
+++ core/hdd/src/wlan_hdd_power.c
@@ -186,6 +186,11 @@ static void hdd_enable_gtk_offload(struct wlan_objmgr_vdev *vdev)
 {
 	QDF_STATUS status;
 
+	if (wlan_vdev_is_open_mode(vdev)) {
+		hdd_debug("Connection in open mode");
+		return;
+	}
+
 	status = ucfg_pmo_enable_gtk_offload_in_fwr(vdev);
 	if (status != QDF_STATUS_SUCCESS)
 		hdd_debug("Failed to enable gtk offload");
@@ -313,6 +318,11 @@ static void hdd_disable_gtk_offload(struct hdd_adapter *adapter,
 {
 	struct pmo_gtk_rsp_req gtk_rsp_request;
 	QDF_STATUS status;
+
+	if (wlan_vdev_is_open_mode(vdev)) {
+		hdd_debug("Connection in open mode");
+		return;
+	}
 
 	/* ensure to get gtk rsp first before disable it*/
 	gtk_rsp_request.callback = wlan_hdd_cfg80211_update_replay_counter_cb;
