dff1deca7b2a6c6b25359a209c1f68d5f3f96f2d
Zhiye Li <lizy90@motorola.com>
2026-01-22 13:58:40 +0800

    qcacld-3.0: Avoid caching NS offload when dynamically disabled
    Currently, the host caches NS offload requests during IPv6 notifier
    work even if NS offload was dynamically disabled earlier. Later, the
    host updates NS offload parameters to firmware as part of IPv4 work.
    If another IPv6 work arrives with extra IP info, it is not sent to
    firmware, causing firmware to ignore NS frames from those IPs.
    
    Fix this by skipping NS offload caching in IPv6 work when NS offload
    is dynamically disabled.
    
    Moto-CRs-fixed: (CR)
    
    Change-Id: I5d99b8fb87f76891b4aa6f421627c41a697f3ae3
    Signed-off-by: Zhiye Li <lizy90@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3732249
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bin Liu <liubin7@motorola.com>
    Submit-Approved: Jira Key

--- components/pmo/dispatcher/inc/wlan_pmo_ucfg_api.h
+++ components/pmo/dispatcher/inc/wlan_pmo_ucfg_api.h
@@ -2390,4 +2390,26 @@ void ucfg_pmo_notify_system_resume(struct wlan_objmgr_psoc *psoc)
 {
 }
 #endif
+
+/**
+ * ucfg_pmo_get_ns_offload_enable_dynamic() - get NS offload dynamic enable
+ * @vdev: vdev objmgr handle
+ *
+ * Return: true is NS offload is dynamically disabled else false
+ */
+bool
+ucfg_pmo_get_ns_offload_enable_dynamic(struct wlan_objmgr_vdev *vdev);
+
+/**
+ * ucfg_pmo_set_ns_offload_enable_dynamic() - Set NS offload dynamic enable
+ * @vdev: vdev objmgr handle
+ * @trigger: pmo trigger
+ * @ns_offload_enable_dyn: NS offload enable dynamic
+ *
+ * Return: None
+ */
+void
+ucfg_pmo_set_ns_offload_enable_dynamic(struct wlan_objmgr_vdev *vdev,
+				       enum pmo_offload_trigger trigger,
+				       bool ns_offload_enable_dyn);
 #endif /* end  of _WLAN_PMO_UCFG_API_H_ */
